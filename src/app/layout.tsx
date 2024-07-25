057076586d67b8c66f0b7211f90e5eeecb452455
Manoj Lasantha <94479824+ItsLasa@users.noreply.github.com>
2024-07-25 21:13:11 +0530

    fix layout bg color  not applying

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import Image from "next/image";
 import couch from "@/assets/couch.svg"
 import { ClerkProvider } from "@clerk/nextjs";
+import UserInf from "@/components/UserInf";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,13 +22,15 @@ export default function RootLayout({
   return (
     <ClerkProvider>
     <html lang="en">
-      <body className={inter.className + "bg-[#17181c] flex flex-col min-h-screen"}>
+      <body className={inter.className + " bg-black flex flex-col min-h-screen"}>
         <header className="w-full">
           <div className=" container border-b p-5 mx-auto flex items-center justify-between">
             <Link  href={"/"} className="flex items-center gap-3">
               <Image src={couch} alt="couch" width={50} height={50}/>
+              <h3 className=" text-white md:text-3xl text-2xl font-bold">AI Room</h3>
             </Link>
               {/* user info component */}
+              <UserInf/>
           </div>
 
         </header>
