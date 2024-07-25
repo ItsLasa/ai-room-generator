b8e8e9457531b78d9a4bb1cb50b38f6cd02049cc
Manoj Lasantha <94479824+ItsLasa@users.noreply.github.com>
2024-07-25 21:27:13 +0530

    create imageslider

--- src/components/CompareSlider.tsx
+++ src/components/CompareSlider.tsx
@@ -1,9 +1,14 @@
 'use client'
 import React from 'react'
+import { ReactCompareSlider,ReactCompareSliderImage } from 'react-compare-slider'
 
 function CompareSlider() {
   return (
-    <div>CompareSlider</div>
+   <ReactCompareSlider
+   itemOne={<ReactCompareSliderImage src='/input.jpg'/>}
+   itemTwo={<ReactCompareSliderImage src='/output.png'/>}
+   
+   />
   )
 }
 
