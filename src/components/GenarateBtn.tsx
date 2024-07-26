34c502abbc61b1cec5d02dbafd958cc63c43d754
Manoj Lasantha <94479824+ItsLasa@users.noreply.github.com>
2024-07-26 22:10:41 +0530

    add button for room genarate

--- src/components/GenarateBtn.tsx
+++ src/components/GenarateBtn.tsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 function GenarateBtn() {
   return (
-    <div>GenarateBtn</div>
+    <button
+    onClick={handleClick}
+    className={`${
+      imageUrl && theme ? "" : "cursor-not-allowed"
+    } p-5 w-full bg-blue-700 text-white rounded-lg hover:opacity-90 active:scale-[.98] transition`}>
+    Generate Room
+  </button>
   )
 }
 
