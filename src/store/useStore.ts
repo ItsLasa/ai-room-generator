3a635b26f9e6e00dffb23dd8583ca5aa21a993fb
Manoj Lasantha <94479824+ItsLasa@users.noreply.github.com>
2024-07-26 21:50:29 +0530

    add useRoom for Selectinput comp

--- src/store/useStore.ts
+++ src/store/useStore.ts
@@ -4,4 +4,10 @@ import { create } from "zustand";
 export const useImage= create((set)=>({
     imageUrl:undefined,
     setImageUrl:(url:string)=>set({imageUrl:url})
-}))
+
+}));
+
+export const useRoom = create((set) => ({
+    room: "living room",
+    setRoom: (room: string) => set({room}),
+  }));
