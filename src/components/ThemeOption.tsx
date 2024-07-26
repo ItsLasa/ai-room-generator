68cfa5abb5f57e4ea351e81df327df760153404b
Manoj Lasantha <94479824+ItsLasa@users.noreply.github.com>
2024-07-26 21:53:39 +0530

    import images from assets

--- src/components/ThemeOption.tsx
+++ src/components/ThemeOption.tsx
@@ -1,5 +1,12 @@
 'use client'
 import React from 'react'
+import coastal from "@/assets/coastal.png";
+import modern from "@/assets/modern.png";
+import professional from "@/assets/professional.png";
+import tribal from "@/assets/tribal.png";
+import tropical from "@/assets/tropical.png";
+import vintage from "@/assets/vintage.png";
+import Image from 'next/image';
 
 function ThemeOption() {
   return (
