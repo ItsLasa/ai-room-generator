c516eeb0f3757701d4dc3eb435ea7cded1d4f1a1
Manoj Lasantha <94479824+ItsLasa@users.noreply.github.com>
2024-07-26 21:42:41 +0530

    create option objects

--- src/components/SelectInput.tsx
+++ src/components/SelectInput.tsx
@@ -1,6 +1,17 @@
 import React from 'react'
+import Select from 'react-select'
 
 function SelectInput() {
+
+
+    const options = [
+        { value: "living room", label: "Living Room" },
+        { value: "bedroom", label: "Bedroom" },
+        { value: "bathroom", label: "Bathroom" },
+        { value: "kitchen", label: "Kitchen" },
+      ];
+
+      
   return (
     <div>SelectInput</div>
   )
