5bbbf5718fa8e4b0f1db5715f3f9c84c7fbe48b2
Manoj Lasantha <94479824+ItsLasa@users.noreply.github.com>
2024-07-26 22:16:53 +0530

    add setLoading,setGenaration

--- src/components/GenarateBtn.tsx
+++ src/components/GenarateBtn.tsx
@@ -1,20 +1,26 @@
-import { useImage, useOutput, useRoom, useTheme } from '@/store/useStore';
-import React from 'react'
+"use client";
+import {
+  useImage,
+  useLoading,
+  useOutput,
+  useRoom,
+  useTheme,
+} from "@/store/useStore";
+import React from "react";
 
-function GenarateBtn() {
-
+function GenerateBtn() {
   const imageUrl = useImage((state: any) => state.imageUrl);
   const theme = useTheme((state: any) => state.theme);
   const room = useRoom((state: any) => state.room);
   const setOutput = useOutput((state: any) => state.setOutput);
-  const setLoading = setLoading((state: any) => state.setLoading);
-  const setGenerating = setLoading((state: any) => state.setGenerating);
+  const setLoading = useLoading((state: any) => state.setLoading);
+  const setGenerating = useLoading((state: any) => state.setGenerating);
 
   async function handleClick() {
     if (imageUrl && theme) {
       setLoading(true);
       setGenerating(true);
-      const res = await fetch("/api/dream", {
+      const res = await fetch("/api/roomGen", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -25,16 +31,21 @@ function GenarateBtn() {
           imageUrl,
         }),
       });
-      
+
+      const newPhoto = await res.json();
+      setLoading(false);
+      setOutput(newPhoto[1]);
+    }
+  }
   return (
     <button
-    onClick={handleClick}
-    className={`${
-      imageUrl && theme ? "" : "cursor-not-allowed"
-    } p-5 w-full bg-blue-700 text-white rounded-lg hover:opacity-90 active:scale-[.98] transition`}>
-    Generate Room
-  </button>
-  )
+      onClick={handleClick}
+      className={`${
+        imageUrl && theme ? "" : "cursor-not-allowed"
+      } p-5 w-full bg-blue-700 text-white rounded-lg hover:opacity-90 active:scale-[.98] transition`}>
+      Generate Room
+    </button>
+  );
 }
 
-export default GenarateBtn
+export default GenerateBtn;
